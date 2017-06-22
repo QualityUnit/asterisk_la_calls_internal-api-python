e20a787ade94633cd8d7d9cbb916c38ac9eefb10
Michal Bebjak <mbebjak@qualityunit.com>
2017-06-22 13:58:45 +0200

    Initial commit

---
+++ liveagent_calls_internal_api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# import models into sdk package
+from .models.call import Call
+from .models.error_response import ErrorResponse
+from .models.ok_response import OkResponse
+
+# import apis into sdk package
+from .apis.calls_api import CallsApi
+
+# import ApiClient
+from .api_client import ApiClient
+
+from .configuration import Configuration
+
+configuration = Configuration()
---
+++ liveagent_calls_internal_api/models/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# import models into model package
+from .call import Call
+from .error_response import ErrorResponse
+from .ok_response import OkResponse
