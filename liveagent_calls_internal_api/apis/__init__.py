d1c45e2b4e7fd861f95d9d726bb226f03d842c10
ArtemFo <afomenko@qualityunit.com>
2022-10-03 18:22:42 +0300

    feat: Add healthcheck endpoint
    #14030

--- liveagent_calls_internal_api/apis/__init__.py
+++ liveagent_calls_internal_api/apis/__init__.py
@@ -2,4 +2,5 @@ from __future__ import absolute_import
 
 # import apis into api package
 from .callsinternal_api import CallsinternalApi
+from .healthcheck_api import HealthcheckApi
 from .sounds_api import SoundsApi
