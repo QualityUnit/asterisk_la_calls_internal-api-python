9996de660a9bc501d9c7e716354be764dff0d51d
ArtemFo <afomenko@qualityunit.com>
2022-10-18 15:01:44 +0300

    fix: Revert path commit
    #14030

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -XX:MaxPermSize=256M -Xmx1024M -jar "/home/adminname/Documents/qu/swagger-codegen-cli.jar" generate -i "/home/adminname/www/develop/asterisk-la/server/swagger/calls_internal.yaml" -l python -o "." -c ./client.json
+java -XX:MaxPermSize=256M -Xmx1024M -jar "../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar" generate -i "../LiveAgent/asterisk-la/server/swagger/calls_internal.yaml" -l python -o "." -c ./client.json
